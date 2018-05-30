c43f1dc963c6fa26e6b813b528d489f283e008fc
MemoryReload <heping_tsdwx@163.com>
2018-05-30 18:17:06 +0800

    add some test code

--- RACPlayground/RACPlayground/ViewController.m
+++ RACPlayground/RACPlayground/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <ReactiveObjC/ReactiveObjC.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSArray* array = @[@(1), @(2), @(3), @(4), @(5), @(6)];
+    RACSequence* sequence = [array rac_sequence];
+    [sequence map:^id _Nullable(id  _Nullable value) {
+        return @(pow([(NSNumber*)value integerValue], 2));
+    }];
+    NSLog(@"pow array: %@",[sequence array]);
+    
+    NSLog(@"even array: %@",[[[array rac_sequence] filter:^BOOL(id  _Nullable value) {
+        return [(NSNumber*)value integerValue] % 2 == 0;
+    }] array]);
+    
+    NSLog(@"fold string : %@",[[[array rac_sequence] map:^id _Nullable(id  _Nullable value) {
+        return [(NSNumber*)value stringValue];
+    }] foldLeftWithStart:@"" reduce:^id _Nullable(id  _Nullable accumulator, id  _Nullable value) {
+        return [(NSString*)accumulator stringByAppendingString:(NSString*)value];
+    }]);
 }
 
 
